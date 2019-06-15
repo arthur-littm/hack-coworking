7020193b077d9564672d72a0070f8ecea342c089
Benjamin Baranger <baranger.benjamin@gmail.com>
2019-06-15 12:00:31 +0100

    add desks to the db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_15_100651) do
+ActiveRecord::Schema.define(version: 2019_06_15_105802) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,12 +39,19 @@ ActiveRecord::Schema.define(version: 2019_06_15_100651) do
     t.index ["user_id"], name: "index_booking_rooms_on_user_id"
   end
 
+  create_table "desks", force: :cascade do |t|
+    t.string "name"
+    t.bigint "area_id"
+    t.index ["area_id"], name: "index_desks_on_area_id"
+  end
+
   create_table "rooms", force: :cascade do |t|
     t.string "name"
     t.integer "capacity"
     t.bigint "area_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "available", default: false
     t.index ["area_id"], name: "index_rooms_on_area_id"
   end
 
