c1efd3182ab333b75169bbff66db841af772aebc
Arthur Littmann <arthur@lewagon.org>
2019-06-15 14:21:27 +0100

    added footer and page for user

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+
+  get 'users/:id', to: "users#show", as: :user_account
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_15_110940) do
+ActiveRecord::Schema.define(version: 2019_06_15_130116) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -80,7 +80,7 @@ ActiveRecord::Schema.define(version: 2019_06_15_110940) do
     t.datetime "updated_at", null: false
     t.string "name"
     t.string "job"
-    t.integer "balance"
+    t.integer "balance", default: 100
     t.boolean "has_dog", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
