313ace17fa24fd3f428dc2565bc31ce38d0bd4c7
Alexander Haumer <alexander.haumer@me.com>
2019-06-15 20:57:13 +0100

    added photo to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_15_183025) do
+ActiveRecord::Schema.define(version: 2019_06_15_195455) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -119,6 +119,7 @@ ActiveRecord::Schema.define(version: 2019_06_15_183025) do
     t.string "job"
     t.integer "balance", default: 100
     t.boolean "has_dog", default: false
+    t.string "photo"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
