6075e01a0e204813a21f82f0b7e526a1bacfb582
Benjamin Baranger <baranger.benjamin@gmail.com>
2019-06-15 11:05:08 +0100

    add foreign key

--- db/migrate/20190615095748_create_area_tags.rb
+++ db/migrate/20190615095748_create_area_tags.rb
@@ -1,8 +1,8 @@
 class CreateAreaTags < ActiveRecord::Migration[5.2]
   def change
     create_table :area_tags do |t|
-      t.references :area
-      t.references :tag
+      t.references :area, foreign_key: true
+      t.references :tag, foreign_key: true
     end
   end
 end
--- db/migrate/20190615095849_create_booking_rooms.rb
+++ db/migrate/20190615095849_create_booking_rooms.rb
@@ -1,8 +1,8 @@
 class CreateBookingRooms < ActiveRecord::Migration[5.2]
   def change
     create_table :booking_rooms do |t|
-      t.references :user
-      t.references :room
+      t.references :user, foreign_key: true
+      t.references :room, foreign_key: true
     end
   end
 end
