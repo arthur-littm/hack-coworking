59cb6e0df7bd35f8c227e0966ad12bad15a10a6a
Arthur Littmann <arthur@lewagon.org>
2019-06-15 13:54:24 +0100

    balance

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def current_balance
+    balance ? balance : 0
+  end
+
 end
