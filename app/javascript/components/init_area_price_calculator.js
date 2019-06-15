b232181e95bff6ac2271f1f7cb32d07912379b5c
Jonny Gates <jonny.gates@outlook.com>
2019-06-15 15:05:21 +0100

    Update price input depending on area preferences

--- app/javascript/components/init_area_price_calculator.js
+++ app/javascript/components/init_area_price_calculator.js
@@ -1,12 +1,29 @@
-const priceCalculatorForm = document.querySelector('#area-price-calculator');
+const getPreferenceWeightings = () => {
+  let sum = 0;
+  document.querySelectorAll('.active').forEach(function(element) {
+    sum += parseFloat(element.dataset.weighting) + 0.25;
+  })
+  return sum;
+}
 
-const updatePrice = (price) => {
-  document.querySelector('#price').innerHTML = price
+const updatePrice = () => {
+  let price = 95;
+  let multiplier = 0;
+  const numberOfPreferences = document.querySelectorAll('.area-preference.active').length;
+
+  multiplier += getPreferenceWeightings();
+  multiplier = (multiplier === 0) ? 1 : (multiplier / 100) + 1
+
+  document.querySelector('#price').value = Math.round(price * multiplier)
 }
 
 const initAreaPriceCalculator = () => {
-  console.log(priceCalculatorForm);
-  updatePrice(25);
+  document.querySelectorAll('.area-preference').forEach(function(button) {
+    button.addEventListener('click', () => {
+      button.classList.toggle('active');
+      updatePrice();
+    })
+  })
 }
 
 export { initAreaPriceCalculator }
