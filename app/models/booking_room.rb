c0f9995af18a91f7c675df2a6dba9c3b3999e8bc
Alexander Haumer <alexander.haumer@me.com>
2019-06-15 15:10:00 +0100

    room_available?

--- app/models/booking_room.rb
+++ app/models/booking_room.rb
@@ -1,13 +1,15 @@
 class BookingRoom < ActiveRecord::Base
   belongs_to :room
   belongs_to :user
+  validates :starts_at, presence: true
+  validates :ends_at, presence: true
   validate :room_available?
 
   private
 
   def room_available?
     is_valid = self.room.unavailability_rooms.select do |unavailability|
-      unavailability.starts_at == self.starts_at && unavailability.ends_at == self.ends_at
+      (self.starts_at > unavailability.starts_at && self.starts_at < unavailability.ends_at) || (self.ends_at > unavailability.starts_at && self.ends_at < unavailability.ends_at)
     end
     errors.add(:booking_room, "Room is not available") unless is_valid.empty?
   end
--- app/models/unavailability_room.rb
+++ app/models/unavailability_room.rb
@@ -1,3 +1,6 @@
 class UnavailabilityRoom < ApplicationRecord
   belongs_to :room
+
+  validates :starts_at, presence: true
+  validates :ends_at, presence: true
 end
