a3f85c76f1b11f91ff1479de9469b87bfaf188bc
Jonny Gates <jonny.gates@outlook.com>
2019-06-15 19:44:11 +0100

    Select one fix

--- app/javascript/packs/checkboxes.js
+++ app/javascript/packs/checkboxes.js
@@ -1,6 +1,15 @@
-const checkboxes = document.querySelectorAll(".booking-choice");
+const checkboxes = document.querySelectorAll(".select-one");
+
+const updateActive = (selected) => {
+  checkboxes.forEach((c) => {
+    (c === selected) ? c.classList.add('active') : c.classList.remove('active')
+  })
+}
+
 checkboxes.forEach((c) => {
   c.addEventListener("click", (e) => {
-    e.currentTarget.classList.toggle("active")
+    updateActive(e);
   })
 });
+
+
