e42821886e5cef8dfb3cd3584ebda2922a766144
Arthur Littmann <arthur@lewagon.org>
2019-06-15 21:06:55 +0100

    added info

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :booking_rooms
+
   def current_balance
     balance ? balance : 0
   end
